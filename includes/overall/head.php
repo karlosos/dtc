6f57796369e16143fff01306821d1d8f3859ea97
karlosos <karlososhd@gmail.com>
2014-04-09 15:52:46 +0200

    mobile css

--- includes/overall/head.php
+++ includes/overall/head.php
@@ -9,6 +9,9 @@
 <html>
 <head>
 	<title> DTC </title>
-	<link rel="stylesheet" href="/dtc/styles/style.css">
+        <link rel="stylesheet" href="/dtc/styles/style.css" media="screen"/>
+        <link rel="stylesheet" href="/dtc/styles/handheld.css" media="handheld,
+        only screen and (max-device-width:480px)"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
 	<meta charset="utf-8">
 </head>
